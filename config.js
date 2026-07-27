61beccd41daa2c1aa7c8cda201f879fd403f10c1
Ukrsedo <ukrsedo@gmail.com>
2026-07-27 11:56:07 +0400

    Update config.js - remove exposed URLs

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PORTAL_CONFIG = {
-  submitAssessmentUrl: 'https://61c2a7fbc9c6e832b745d32be0447b.45.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/08/workflows/c3f13e2cdb8f4eb2ad8bf842f6006a3f/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ypTo217-2398OgdqfoY-1Jxnxk0Ey_KFDk5OFO8kmpw',
-  getStudentResultsUrl: 'https://61c2a7fbc9c6e832b745d32be0447b.45.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/30/workflows/f3360f38c4204cbd9a2e4a1bd17e4094/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=-_LBY09QMH7P8vvouMxAZt7StkEVkHY2quIhg_IGSMk'
+  submitAssessmentUrl: '',
+  getStudentResultsUrl: ''
 };
